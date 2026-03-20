dad5424b51a125ca259c705a154007d812a8b30c
LuizITDev <luiz.job@outlook.com>
2026-03-20 19:22:13 -0300

    Projeto Biblioteca

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Biblioteca</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    include("menu.php");
+    ?>
+    <div class="content">
+        <h1>Biblioteca Digital Muncicipal</h1>
+        <p>A Biblioteca Digital Muncicipal tem como missão promover o acesso ao conhecimento, à cultura e a à educação por meio de um acervo digital acessível a toda comunidade  </p>
+        <br>
+        <p>Nosso sistema permite consultar obras literárias, acessar livros digitais e incentivar a leitura através da tecnologia.
+            Este ambiente foi desenvolvido para facilitar o gerenciamento do acervo bibliográfico e aproximar leitores do universo literário.
+        </p>
+        <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcXNsa3Nzc3QxZXB5OTF4OWJqc2Fua2I0OWVydWJ0b2N5N29sazZjNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9dg/DQxDWGlQKvDH2trDGK/giphy.gif" alt="Homem Pegando Livro" style="width:100%;border: radius 12px;">
+</body>
+    </div>
+    
+</html>
